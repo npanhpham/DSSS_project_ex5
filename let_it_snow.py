fc6c7a8ec06cf4f66d6585d354c8d24423ca6399
Anh Pham <pphuonganh2512@gmail.com>
2022-12-04 21:43:48 +0100

    file let_it_snow.py has added colors

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,5 +1,6 @@
 import turtle
 import numpy as np
+import random 
 
 
 def main(speed=0, bg_color="grey"):
@@ -14,7 +15,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
-
+    color_list = ["red", "pink", "blue", "green"]
 
     for _ in range(10):
         # define some params
@@ -22,6 +23,8 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
+        color = random.choice(color_list)
+        myTurtle.pencolor(color)	
 
         # Go to the start position of the snowflake
         myTurtle.penup()
